d10b5135feaf7ab3a5871addc7c52b90e29fc7ec
Bogdan <bohdan.ulich@gmail.com>
2022-01-11 10:16:59 +0200

    popup2

--- src/components/Sort-popup/Sort.jsx
+++ src/components/Sort-popup/Sort.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import { faSortDown} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from "react/cjs/react.development"
+import { useEffect, useState } from "react/cjs/react.development"
 
 function Sort() {
 
     const [visiblePopup, setVisiblePopup] = useState(false)
+
+    const sortRef = useRef()
+
+    const handleOutsideClick = (e) => {
+        if(!e.path.includes(sortRef.current)){
+            setVisiblePopup(false)
+        }
+    }
+    
+    useEffect(() => {
+        document.body.addEventListener('click', handleOutsideClick)
+    }, []) 
 
     const toggleVisiblePopup = () => {
         setVisiblePopup(!visiblePopup)
     }
 
     return(
-        <div className="sort">
+        <div className="sort" ref={sortRef}>
             <div className="sort__label" onClick={toggleVisiblePopup}><FontAwesomeIcon className="sort__icon" icon={faSortDown} />Сортировка по:<span>популярности</span></div>
             {visiblePopup && 
             <ul className="sort__popup">
