295b90e0f470ba9985a664c57d84989f73398035
Богдан <bohdan.ulich@gmail.com>
2022-02-14 18:14:24 +0200

    carteducer

---
+++ src/redux/actions/cart.js
@@ -0,0 +1,10 @@
+export const setTotalPrice = (items) => ({
+    type: 'SET_TOTAL_PRICE',
+    payload: items,
+})
+
+export const setTotalCount = (category) => ({
+    type: 'SET_TOTAL_COUNT',
+    payload: category,
+})
+
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import productsReduser from './products'
 import filtersReduser from './filters'
 import leftMenuReduser from './leftMenu'
 import categoryReduser from './category'
+import cartReduser from './cart'
 
 const rootReducer = combineReducers({
     products: productsReduser,
     filters: filtersReduser,
     leftMenu : leftMenuReduser,
     categories: categoryReduser,
+    cart: cartReduser
 })
 
 export default rootReducer
