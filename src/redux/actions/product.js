9f3b169fc881757260720e5cbfb240e3ae4c2d63
Yurii <yurban1717@gmail.com>
2022-02-23 13:30:22 +0200

    fix

--- src/redux/actions/product.js
+++ src/redux/actions/product.js
@@ -1,52 +1,22 @@
 import axios from 'axios';
 
 export const fetchProduct = (id) => (dispatch) => {
-    /*dispatch(setLoaded(false))
-    axios.get(`/disposablePods?id=${id}`).then( ({ data }) => {
-        dispatch(setProducts(data))
-    });*/
-
     dispatch(setLoaded(false));
-    axios({
-        method: 'get',
-        url: 'http://elfbar-shop/?action=getProduct&id='+id,
-        data: '',
-        config: {headers: {'Content-Type': 'multipart/form-data'}}
-    }).then(function (response) {
+    axios.get('http://elfbar-shop/?action=getProduct&id='+id).then(function (response) {
         dispatch(setProducts([response.data]));
     });
 }
 
 export const fetchProducts = (category, sortBy) => (dispatch) => {
-    /*dispatch(setLoaded(false))
-    axios.get(`/disposablePods?category=${category}&_sort=${sortBy.type}&_order=${sortBy.order}`).then( ({ data }) => {
-            dispatch(setProducts(data))
-     })*/
-
-    dispatch(setLoaded(false))
-    axios({
-        method: 'get',
-        url: 'http://elfbar-shop/?action=getCategoryProducts&category='+category+'&sort='+sortBy.type,
-        data: '',
-        config: {headers: {'Content-Type': 'multipart/form-data'}}
-    }).then(function (response) {
-        dispatch(setProducts(response.data))
+    dispatch(setLoaded(false));
+    axios.get('http://elfbar-shop/?action=getCategoryProducts&category='+category+'&sort='+sortBy.type).then(function (response) {
+        dispatch(setProducts(response.data));
     });
 }
 
 export const fetchPopularProducts = () => (dispatch) => {
-    /*dispatch(setLoaded(false))
-    axios.get('/disposablePods?rating=5&available=1').then( ({ data }) => {
-            dispatch(setProducts(data))
-    });*/
-
-    dispatch(setLoaded(false))
-    axios({
-        method: 'get',
-        url: 'http://elfbar-shop/?action=getPopularProducts',
-        data: '',
-        config: {headers: {'Content-Type': 'multipart/form-data'}}
-    }).then(function (response) {
+    dispatch(setLoaded(false));
+    axios.get('http://elfbar-shop/?action=getPopularProducts').then(function (response) {
         dispatch(setProducts(response.data))
     });
 }
